4802e7e29359b3ebf4cd1faafcd0058c2b22d08d
devabhimanyu <dev.chauhan.singh.abhimanyu@gmail.com>
2024-05-19 18:43:43 +0900

    :sparkles: Hx delete, hx-on, hx disabled and clean up

--- app.js
+++ app.js
@@ -1,54 +1,83 @@
-import express from "express";
+import express from 'express';
 
-import { HTMX_KNOWLEDGE } from "./data/htmx-info.js";
+const courseGoals = [];
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
-app.get("/info", (_req, res) => {
+const renderGoalItem = (goal)=>{
+  return `
+  <li id="goal-${goal.id}">
+    <span>${goal.desc}</span>
+    <button 
+    hx-delete="/goals/${goal.id}" 
+    hx-target="#goal-${goal.id}"
+  >
+    Remove
+  </button>
+  </li>
+`
+}
+
+app.get('/', (req, res) => {
   res.send(`
-    <ul>
-      ${HTMX_KNOWLEDGE.map((infoText) => `<li>${infoText}</li>`).join("")}
-    </ul>
-  `);
-});
-
-app.get("/", (_req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>HTMX Essentials</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-          
-        />
-        <script src="/htmx.min.js" defer></script>
-        <link rel="icon" href="/icon.png" />
-        <link rel="stylesheet" href="/main.css" />
-      </head>
-      <body>
-        <header id="main-header">
-          <img src="/htmx-logo.jpg" alt="HTMX Logo" />
-          <h1>Essentials</h1>
-        </header>
-
-        <main>
-          <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
-          <button 
-            hx-get="/info" 
-            hx-trigger="mouseenter[ctrlKey],click"
-            hx-target="main" 
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Learn HTMX</title>
+      <link rel="stylesheet" href="/main.css" />
+      <script src="/htmx.js" defer></script>
+    </head>
+    <body>
+      <main>
+        <h1>Manage your course goals</h1>
+        <section>
+          <form 
+            id="goal-form" 
+            hx-post="/goals" 
+            hx-target="#goals"
             hx-swap="beforeend"
+            hx-on::after-request="this.reset()"
+            hx-disabled-elt="form button">
+            <div>
+              <label htmlFor="goal">Goal</label>
+              <input type="text" id="goal" name="goal" />
+            </div>
+            <button type="submit">Add goal</button>
+          </form>
+        </section>
+        <section>
+          <ul 
+            id="goals" 
+            hx-swap="outerHTML"
+            hx-confirm="Are You Sure?"
           >
-          Learn More
-          </button>
-        </main>
-      </body>
-    </html>
+          ${courseGoals.map(renderGoalItem).join('')}
+          </ul>
+        </section>
+      </main>
+    </body>
+  </html>
   `);
 });
 
+app.post('/goals', (req, res) => {
+  const goalText = req.body.goal;
+  const newGoal = {id: Date.now().toString(), desc:goalText}
+  courseGoals.push(newGoal);
+  setTimeout(()=>{
+    res.send(renderGoalItem(newGoal));
+  },2000)
+});
+
+app.delete('/goals/:id', (req, res) => {
+  const id = req.params.id;
+  const index = courseGoals.findIndex((goal) => goal.id === id);
+  courseGoals.splice(index, 1);
+  res.send();
+});
 app.listen(3000);
